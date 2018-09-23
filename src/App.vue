99facbef067172cc24671a483fe4f10c8fb655f4
Natalie Schulz <natalie.schulz@me.com>
2018-09-23 13:34:55 -0700

    added new dist folder & css reset"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img id="profile-pic" alt="Vue logo" src="./assets/logo.png">
+    <img id="profile-pic" alt="mouse logo" src="./assets/logo.png">
     <HelloWorld msg="Hi, my name is Natalie"/>
   </div>
 </template>
@@ -17,17 +17,41 @@ export default {
 </script>
 
 <style>
+html, body, div, span, iframe,
+h1, h2, h3, h4, h5, h6, p,
+a, img, strong, ol, ul, li, form,
+label, legend, table, footer, header,
+menu, nav, output, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+}
+
+body {
+	line-height: 1;
+}
+
+ol, ul {
+	list-style: none;
+}
+
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  text-align: center;
 }
 
-#profile-pic {
-  max-height: 200px;
-  border-radius: 50%;
-}
 </style>
