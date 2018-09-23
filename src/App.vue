088e5898662e9e5b1baf6931cf89ca699bbc15d3
Natalie Schulz <natalie.schulz@me.com>
2018-09-23 12:22:02 -0700

    inital vue setup

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img id="profile-pic" alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Hi, my name is Natalie"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#profile-pic {
+  max-height: 200px;
+  border-radius: 50%;
+}
+</style>
